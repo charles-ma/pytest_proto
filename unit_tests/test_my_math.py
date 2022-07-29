db2df2e2a5631e24baad75949aa6b8d0086479d8
Charles <mcrichard.buaa@gmail.com>
2022-07-29 00:11:24 +0000

    parametrize arith unit tests

--- unit_tests/test_my_math.py
+++ unit_tests/test_my_math.py
@@ -4,14 +4,18 @@ from my_math.arithmetic import sum, prod, div, sub
 @pytest.mark.unit
 class TestUnit():
 
-  def test_sum(self):
-    assert sum(1, 1) == 2
+  @pytest.mark.parametrize('x, y, expected', [(1, 1, 2), (1, 2, 3), (2, 1, 3)])
+  def test_sum(self, x, y, expected):
+    assert sum(x, y) == expected
 
-  def test_prod(self):
-    assert prod(2, 3) == 6
+  @pytest.mark.parametrize('x, y, expected', [(1, 1, 1), (2, 3, 6), (-1, -5, 5)])
+  def test_prod(self, x, y, expected):
+    assert prod(x, y) == expected
 
-  def test_div(self):
-    assert div(1, 2) == 0.5
+  @pytest.mark.parametrize('x, y, expected', [(1, 1, 1), (2, 0.2, 10), (-5, -2, 2.5)])
+  def test_div(self, x, y, expected):
+    assert div(x, y) == expected
 
-  def test_sub(self):
-    assert sub(2, 1) == 1
+  @pytest.mark.parametrize('x, y, expected', [(1, 1, 0), (2, 3, -1), (5, -10, 15)])
+  def test_sub(self, x, y, expected):
+    assert sub(x, y) == expected
