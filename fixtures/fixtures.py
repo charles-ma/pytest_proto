36ca0069e159d02d8f373729534c9643699ef43e
Charles <mcrichard.buaa@gmail.com>
2022-07-29 00:00:58 +0000

    add ping to yahoo mail

--- fixtures/fixtures.py
+++ fixtures/fixtures.py
@@ -1,6 +1,9 @@
 import pytest
 import smtplib
 
-@pytest.fixture(scope="module")
-def smtp_connection():
-  return smtplib.SMTP("smtp.gmail.com", 587, timeout=5)
+@pytest.fixture(scope="module", params=["smtp.gmail.com", "smtp.mail.yahoo.com"])
+def smtp_connection(request):
+  smtp_connection = smtplib.SMTP(request.param, 587, timeout=5)
+  yield smtp_connection
+  print(f"finalizing {smtp_connection}")
+  smtp_connection.close()
--- integration_tests/test_smtp.py
+++ integration_tests/test_smtp.py
@@ -7,7 +7,6 @@ class TestSMTP(object):
   def test_ehlo(self, smtp_connection):
       response, msg = smtp_connection.ehlo()
       assert response == 250
-      assert b"smtp.gmail.com" in msg
 
   def test_noop(self, smtp_connection):
       response, msg = smtp_connection.noop()
