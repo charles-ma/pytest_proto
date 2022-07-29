57fe1da770652bd0b815684a7f25a8ac37c83246
Charles <mcrichard.buaa@gmail.com>
2022-07-29 18:42:10 +0000

    change log level and add print msg to smtp tests

--- integration_tests/test_smtp.py
+++ integration_tests/test_smtp.py
@@ -6,6 +6,8 @@ class TestSMTP(object):
 
   def test_ehlo(self, smtp_connection):
       response, msg = smtp_connection.ehlo()
+      # this console output will only be shown if the test fails
+      print("received message:" + str(msg))
       assert response == 250
 
   def test_noop(self, smtp_connection):
